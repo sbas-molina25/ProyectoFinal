cc914a0be8c7e1ad526aa9481b16dc1fb1b4d990
SebasMolina25 <sebasmolina352@gmail.com>
2025-07-14 17:54:10 -0500

    Carga Inicial

---
+++ src/main/java/com/epn/practice/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.epn.practice.proyectofinal;
+
+/**
+ *
+ * @author Sebas
+ */
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
